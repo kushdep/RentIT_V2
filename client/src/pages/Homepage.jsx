5cf547fedfa9b88089bc694cf8142b1fa0930029
Deependra kumar <kushdep017@gmail.com>
2025-07-11 14:15:25 +0530

    rent locs page styling

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,7 +1,34 @@
-function Homepage(){
-    return <>
-        <h1>Homepage</h1>
+function Homepage() {
+  return (
+    <>
+      <header>
+        <div className="container-fluid mx-0">
+          <div className="row">
+            <div className="col p-0">
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col"></div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col"></div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row"><div className="col"></div></div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col"></div>
+          </div>
+        </div>
+      </header>
     </>
+  );
 }
 
-export default Homepage
+export default Homepage;
--- client/src/pages/RentLocs.jsx
+++ client/src/pages/RentLocs.jsx
@@ -1,3 +1,11 @@
-export default function RentLocs(){
-    return <h1>Rental Locations</h1>
-} 
+export default function RentLocs() {
+  return (
+    <>
+      <header>
+        <div className="page-heading image-fluid p-">
+          <img src="/images/rent-locs-homepage.png" alt="" />
+        </div>
+      </header>
+    </>
+  );
+}
