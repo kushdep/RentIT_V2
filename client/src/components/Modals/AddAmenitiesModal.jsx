3a8f5d69998079147e9ca0e7a6d17eee45dba844
Deependra kumar <kushdep017@gmail.com>
2025-07-18 17:16:28 +0530

    add ammenities completed

--- client/src/components/Modals/AddAmenitiesModal.jsx
+++ client/src/components/Modals/AddAmenitiesModal.jsx
@@ -1,11 +1,14 @@
 import { Ammentities } from "../../config";
 import { addLocActions } from "../../store/addLoc-slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function AddAmmenitiesModal({ reference, id }) {
+  const offAmm = useSelector((state) => state.addLocData.offAmm);
+  const mmStt = offAmm.find((e) => e.id === id)?.opt; //got the options array
   const dispatch = useDispatch();
-  const ammenityData = Ammentities.find((e)=>e.id===id)
-  console.log(ammenityData)
+  const clss = "";
+  const ammenityData = Ammentities.find((e) => e.id === id);
+  console.log(ammenityData);
 
   return (
     <>
@@ -36,6 +39,13 @@ function AddAmmenitiesModal({ reference, id }) {
                       className="col-6 btn-check"
                       id={e.id}
                       autocomplete="off"
+                      checked={
+                        mmStt
+                          ? mmStt.find((m) => e.id === m.id)
+                            ? true
+                            : false
+                          : false
+                      }
                       onChange={(event) =>
                         dispatch(
                           addLocActions.handleAmmOpt({
@@ -46,18 +56,26 @@ function AddAmmenitiesModal({ reference, id }) {
                         )
                       }
                     />
-                    <label
-                      className="btn btn-outline-dark rounded-4 p-3 my-2"
-                      htmlFor={e.id}
-                    >
-                      <img
-                        src={`/public/${e.img}`}
-                        alt=""
-                        className="me-3"
-                        style={{ width: 50, height: 50, objectFit: "cover" }}
-                      />
-                      {e.name}
-                    </label>
+                    {
+                      <label
+                        className={
+                          mmStt
+                            ? mmStt.find((m) => e.id === m.id)
+                              ? "btn btn-outline-dark rounded-4 p-3 my-2"
+                              : "btn p-3 my-2"
+                            : "btn p-3 my-2"
+                        }
+                        htmlFor={e.id}
+                      >
+                        <img
+                          src={`/public/${e.img}`}
+                          alt=""
+                          className="me-3"
+                          style={{ width: 50, height: 50, objectFit: "cover" }}
+                        />
+                        {e.name}
+                      </label>
+                    }
                     <br></br>
                   </>
                 );
