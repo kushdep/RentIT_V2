a4ce969eead4cb5e8785fd73617bcbc975942b2f
Deependra kumar <kushdep017@gmail.com>
2025-09-08 19:32:14 +0530

    Homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import "../css/homepage.css";
 function Homepage() {
   return (
       <div className="container-fluid">
-        <div className="row p-0 " >
+        <div className="row p-0" >
           <div className="col w-100 p-0 justify-content-end">
               <img
                 src="/public/images/homepage-first-section.png"
